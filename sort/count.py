597c948366d4894af233a314982fea134c151d2b
tmxkqotnl <tmxkqotnl@gmail.com>
2022-06-30 22:09:20 +0900

    fix, remove zip

--- sort/count.py
+++ sort/count.py
@@ -9,6 +9,6 @@ print("정렬 전 : {}".format(lst))
 count = [0] * (N + 1)
 for i in lst:
     count[i] += 1
-ans = [str(i) * j for i, j in zip(range(N + 1), count)]
+ans = [str(i) * count[i] for i in range(N + 1)]
 
 print("정렬 후 : {}".format("".join(ans)))
