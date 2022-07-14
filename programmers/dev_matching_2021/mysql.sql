0e78408aaea2c8a65ec2ace2841a8ab01b118222
tmxkqotnl <tmxkqotnl@gmail.com>
2022-07-14 20:37:14 +0900

    fix, rm - 폴더이전

--- programmers/dev_matching_2021/mysql.sql
+++
@@ -1,3 +0,0 @@
---- test : 헤비 유저가 소유한 장소
---- link : https://programmers.co.kr/learn/courses/30/lessons/77487
-select * from places where host_id in (select host_id from places group by host_id having count(host_id)>1) order by id;
